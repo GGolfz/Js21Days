cc7596a862697550ddb56bff8affa778afe76622
Mr.WISARUT KITTICHAROENPHONNGAM <62130500226@st.sit.kmutt.ac.th>
2020-04-05 15:04:53 +0700

    [ADD] Lab11

---
+++ 11 - Text Reveal/start.js
@@ -0,0 +1,21 @@
+(() => {
+  // เริ่มเขียนโค้ด
+  const onScroll = ()=>{
+    const sectionElems = Array.from(document.querySelectorAll('section'));
+    sectionElems.forEach(sectionElem => {
+      const imgElem = sectionElem.querySelector('img');
+      const textElem = sectionElem.querySelector('.text');
+
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition >= revealPosition) {
+        textElem.classList.add('reveal');
+      }
+    });
+  }
+  const run = ()=>{
+    document.addEventListener('scroll',onScroll);
+  }
+  run();
+})();
